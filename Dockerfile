189068a72130893f120bf348c89d7fe6f74874fe
SarthakAdhikari <adhikari.sarthak@gmail.com>
2020-11-02 12:02:52 +0545

    Fix debugpy address

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 RUN apk update && apk add nmap
 
 COPY . .
-EXPOSE 3000
 
 CMD [ "python", "./test.py" ]
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ a = 5
 
 print("Before attach")
 import debugpy
-debugpy.listen(3000)
+debugpy.listen(("0.0.0.0", 3000))
 debugpy.wait_for_client()
 print("After attach")
 breakpoint()
